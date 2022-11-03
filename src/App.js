f199d6d68d9eca9e3bd696bf49e7eee9733e263c
amitsinha07 <aamitsinha456@gmail.com>
2022-11-03 18:42:37 +0530

    LowerCase button added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import NavComp from './components/NavComp'
 import Textform from './components/Textform';
-import './App.css';
 
 
 function App() {
@@ -8,9 +7,8 @@ function App() {
     <>
       <NavComp title="Text Utils"/>
       <div className='container'>
-      <Textform heading="Enter the text to analyze below"/>
+        <Textform heading="Enter the text to analyze below"/>
       </div>
-      
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,27 +1,50 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 function Textform(props) {
-    const [text, setText] = useState("Enter text");
+  const [text, setText] = useState("Enter text");
 
-    const handleUpText = () => {
-        let newText = text.toUpperCase(text);
-        setText(newText);
-    }
+  const handleUpText = () => {
+    let newText = text.toUpperCase(text);
+    setText(newText);
+  };
 
-    const handleOnChange = (e) =>{
-        setText(e.target.value);
-    }
+  const handleLoText = () => {
+    let newText = text.toLowerCase(text);
+    setText(newText);
+  };
 
-
-
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
 
   return (
     <>
-      <h2>{props.heading}</h2>
-      <div className='mb-3'>
-        <textarea className='form-control' onChange={handleOnChange} value={text} id="my-box" rows="8"></textarea>
+      <div className="container">
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            onChange={handleOnChange}
+            value={text}
+            id="my-box"
+            rows="8"
+          ></textarea>
+        </div>
+        <button onClick={handleUpText} className="btn btn-primary">
+          Convert To Uppercase
+        </button>
+        <button onClick={handleLoText} className="btn btn-primary mx-2">
+          Convert To Lowercase
+        </button>
       </div>
-      <button onClick={handleUpText} className='btn btn-primary'>Convert To Uppercase</button>
+
+      <div className="container my-3">
+        <h2>Your text summery</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
     </>
   );
 }
